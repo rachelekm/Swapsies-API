037fd7902ecdaf4bc692a1f26686bf4f6136007c
Rachele Morino <rachelekm@gmail.com>
2018-07-31 11:48:49 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,92 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require('./config');
+
+const app = express();
+
+const { router: newUserRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+const {router: dreamEntryRouter } = require('./dreams');
+
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/account', newUserRouter);
+app.use('/login', authRouter);
+app.use('/dreams', dreamEntryRouter);
+
+app.use('*', (req, res) => {
+  return res.status(404).sendFile(__dirname + '/public/errorpage.html');
+});
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer, TEST_DATABASE_URL };
+ 
